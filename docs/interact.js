92e955f692459a26d0e6fe9047b989ee8ce41136
epp5198 <112676517+epp5198@users.noreply.github.com>
2023-10-08 23:23:19 -0400

    testing button

--- docs/interact.js
+++ docs/interact.js
@@ -3,3 +3,8 @@ window.addEventListener('DOMContentLoaded',init,false);
 function init() {
     alert('The page loaded!');
 }
+unction init() {
+    alert ('Hi there! Looks like the page loaded! Yay!');
+    var buttons = document.getElementsByTagName("button")
+buttons[0].addEventListener('click', changeColor,false)
+}
