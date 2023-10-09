7fdc3f5955e042c1ae0a57d793152d5e58c7a983
epp5198 <112676517+epp5198@users.noreply.github.com>
2023-10-08 23:37:51 -0400

    Update interact.js

--- docs/interact.js
+++ docs/interact.js
@@ -4,8 +4,7 @@ function init() {
     alert('The page loaded!');
 }
 unction init() {
-    alert ('Hi there! Looks like the page loaded! Yay!');
-    var buttons = document.getElementsByTagName("button")
+    var buttons = document.getElementsByTagName("p")
 buttons[0].addEventListener('click', changeColor,false)
 }
 function changeColor() {
