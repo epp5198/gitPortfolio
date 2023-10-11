2f2bed02355d82e1e3604863bad9fa2994ea3723
epp5198 <112676517+epp5198@users.noreply.github.com>
2023-10-11 16:12:52 -0400

    Update interact.js

--- docs/interact.js
+++ docs/interact.js
@@ -3,11 +3,3 @@ window.addEventListener('DOMContentLoaded',init,false);
 function init() {
     alert('The page loaded!');
 }
-function init() {
-    var buttons = document.getElementsByTagName("p")
-buttons[0].addEventListener('click', changeColor,false)
-}
-function changeColor() {
-var colorMe1 = document.getElementById("colorToggle") 
-{colorMe1.style.backgroundColor = "grey";
-}
